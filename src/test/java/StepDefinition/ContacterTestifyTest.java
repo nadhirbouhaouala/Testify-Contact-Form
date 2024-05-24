61bf2951d964a806c6793c8da03b24fa02e8cab0
unknown <nadhir_bouhaouala@live.fr>
2024-05-24 18:33:47 +0200

    commit num 4

--- src/test/java/StepDefinition/ContacterTestifyTest.java
+++ src/test/java/StepDefinition/ContacterTestifyTest.java
@@ -12,7 +12,7 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
-public class Contacter_TtestifySD {
+public class ContacterTestifyTest {
 	
 	private WebDriver driver;
 	private String baseUrl;
